488b49f97f00b81c7ae95eef2f2cf253fd681b66
Alton <smithiialton@gmail.com>
2024-10-27 11:21:44 -0700

    created skeleton for methods

---
+++ routing-and-single-page-apps/src/components/BrowseCharacters.jsx
@@ -0,0 +1,10 @@
+const BrowseCharacter = () => {
+    return (
+        <div>
+            <h2>Browse Character</h2>
+            <p>This component is under construction. Check back later!</p>
+        </div>
+    );
+};
+
+export default BrowseCharacter;
---
+++ routing-and-single-page-apps/src/components/CharacterDetails.jsx
@@ -0,0 +1,10 @@
+const CharacterDetails = () => {
+    return (
+        <div>
+            <h2>Character Details</h2>
+            <p>This component is under construction. Check back later!</p>
+        </div>
+    );
+};
+
+export default CharacterDetails;
