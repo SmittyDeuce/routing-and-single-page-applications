d3893a9dfad3cb7c1f5f96e26e7b1ea0fb9c0d4c
Alton <smithiialton@gmail.com>
2024-10-27 19:11:44 -0700

    completed home

--- routing-and-single-page-apps/src/components/Home.jsx
+++ routing-and-single-page-apps/src/components/Home.jsx
@@ -1,6 +1,9 @@
+import React from "react";
+import "./Home.css";
+
 const Home = () => {
   return (
-    <div>
+    <div id="main-container">
       <h1>Comic Book Library Project</h1>
       <div id="home-container">
         <p>
@@ -10,24 +13,30 @@ const Home = () => {
           a React app. You'll create a few pages: Home, Browse Characters,
           Character Details, and Comics, using React Router.
         </p>
+        <div id="list-container">
+            <h3>Project Steps:</h3>
+            
+            <div id="ol-container">
+            <ol>
+                <li>Install React Router - Set up React Router in your project.</li>
+                <li>
+                Create Route Components - Build simple components for each page.
+                </li>
+                <li>Set Up Routes - Use routes to navigate between pages.</li>
+            </ol>
+            </div>
+
+            <h3>Bonus:</h3>
+            <div id="ul-container">
+            <ul>
+                <li>Add dynamic routes for character details.</li>
+                <li>Set up a NotFound page to handle unknown routes.</li>
+            </ul>
+            </div>
+        </div>
+
       </div>
 
-      <h3>Project Steps:</h3>
-      <div id="ol-container">
-        <ol>
-          <li>Install React Router - Set up React Router in your project.</li>
-          <li>Create Route Components - Build simple components for each page.</li>
-          <li>Set Up Routes - Use routes to navigate between pages.</li>
-        </ol>
-      </div>
-
-      <h3>Bonus:</h3>
-      <div id="ul-container">
-        <ul>
-            <li>Add dynamic routes for character details.</li>
-            <li>Set up a NotFound page to handle unknown routes.</li>
-        </ul>
-      </div>
     </div>
   );
 };
